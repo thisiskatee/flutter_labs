39217ac6f1339c1825ae52efc50af7c69c3b57f7
Ekaterina Labetova <katya230501@yandex.ru>
2022-03-21 12:12:11 +0300

    first commit

---
+++ labetova_lab_1/lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'my_home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: const MyHomePage(title: 'Try my first app'),
+    );
+  }
+}
